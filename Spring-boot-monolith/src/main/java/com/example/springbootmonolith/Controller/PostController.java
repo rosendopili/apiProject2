0b80544a5dc1cde05640c160abd4abd11af7a69e
Melissa Jeremie <melissajeremie@gmail.com>
2019-10-09 00:20:07 -0400

    expanded endpoints for performing CRUD operations on comments and posts, not yet functional

--- Spring-boot-monolith/src/main/java/com/example/springbootmonolith/Controller/PostController.java
+++ Spring-boot-monolith/src/main/java/com/example/springbootmonolith/Controller/PostController.java
@@ -1,4 +1,31 @@
 package com.example.springbootmonolith.Controller;
 
+import com.example.springbootmonolith.Service.PostService;
+import com.example.springbootmonolith.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users/{username}/{post_id}")
 public class PostController {
+
+    @Autowired
+    public PostService postService;
+
+    @PostMapping("/user/{username}/{postId}")
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
+    }
+    @GetMapping("/user/{username}/{postId}/list")
+    public Iterable<Post> listPosts() {
+        return postService.listPosts();
+    }
+
+    @DeleteMapping("/user/{username}/{postId}")
+    public HttpStatus deletePost(long postId) {
+        return postService.deletePost(postId);
+    }
+
+
 }
